a510658786634a012076931b710fd259d3607722
Wasiu <nexxyb@gmail.com>
2026-06-08 17:25:45 +0100

    first commit

--- apps/web/stubs/empty-kysely-adapter.mjs
+++
@@ -1,9 +0,0 @@
-export function getKyselyDatabaseType() {
-  return null;
-}
-
-export async function createKyselyAdapter() {
-  return { kysely: null, databaseType: null, transaction: undefined };
-}
-
-export default {};
